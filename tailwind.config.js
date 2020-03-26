324105191c13532d2d7f1e5d4e41f004628cf075
Justin Spradlin <jspradlin@gmail.com>
2020-03-26 17:19:31 -0400

    Initial Page Layout and Documentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   variants: {},
   plugins: [
